37a42aede700daf8cafa9de1fe9d4998c8f80481
sankar58 <ayudhamsankar@gmail.com>
2026-01-22 21:44:13 +0530

    list adding

---
+++ list/adding.py
@@ -0,0 +1,13 @@
+my_list=[1,2,3,4]
+
+my_list.append(5)
+print(my_list)
+
+my_list=[1,2,3,4]
+my_list.insert(0,0)
+print(my_list)
+
+my_list=[1,2,3,4]
+new_list=[5,6,7,8]
+my_list.extend(new_list)
+print(my_list)
