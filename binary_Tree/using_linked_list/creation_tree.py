a820435c2e94e9f9dbe5d798db797b322f4f40d3
sankar58 <ayudhamsankar@gmail.com>
2026-01-25 19:25:13 +0530

    creation of tree using linkedlist

---
+++ binary_Tree/using_linked_list/creation_tree.py
@@ -0,0 +1,21 @@
+class TreeNode:
+    def __init__(self,data,children=[]):
+        self.data=data
+        self.children=children
+
+    def __str__(self,level=0):
+        ret=" "*level+str(self.data)+"\n"
+        for child in self.children:
+            ret += child.__str__(level+1)
+        return ret
+
+
+    def addchild(self,TreeNode):
+        self.children.append(TreeNode)
+
+tree=TreeNode("drinks",[])
+cold=TreeNode("cold",[])
+hot=TreeNode("hot",[])
+tree.addchild(cold)
+tree.addchild(hot)
+print(tree)
