cd719cf094409f98c5811088afa034467f092461
sankar58 <ayudhamsankar@gmail.com>
2026-01-25 19:24:17 +0530

    creation of binary tree using linked list

---
+++ binary_Tree/using_linked_list/creation_binary_tree.py
@@ -0,0 +1,8 @@
+class TreeNode:
+    def __init__(self,data):
+        self.data=data
+        self.leftchild=None
+        self.rightchild=None
+
+binaryTree=TreeNode("drinks")
+print(binaryTree.data)
