fe43e203b556e62ed59e5c1a4835d309cea4772d
sankar58 <ayudhamsankar@gmail.com>
2026-01-26 20:08:49 +0530

    binary levelorder

---
+++ binary_Tree/using_linked_list/level_order.py
@@ -0,0 +1,27 @@
+import myqueue
+class TreeNode:
+    def __init__(self,data):
+        self.data=data
+        self.leftchild=None
+        self.rightchild=None
+
+binaryTree=TreeNode("drinks")
+leftchild=TreeNode("hot")
+rightchild=TreeNode("cold")
+binaryTree.leftchild=leftchild
+binaryTree.rightchild=rightchild
+
+def levelorder(rootnode):
+    if not rootnode:
+        return
+    else:
+        customqueue = myqueue.Queue()
+        customqueue.enqueue(rootnode)
+        while not customqueue.isEmpty():
+            root = customqueue.dequeue()
+            print(root.value.data)
+            if root.value.leftchild is not None:
+                customqueue.enqueue(root.value.leftchild)
+            if root.value.rightchild is not None:
+                customqueue.enqueue(root.value.rightchild)
+levelorder(binaryTree)
