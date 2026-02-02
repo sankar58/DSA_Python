04fa3775ebadf481ae96de1f38df51eb2a2b91a1
sankar58 <ayudhamsankar@gmail.com>
2026-02-02 22:04:45 +0530

    neetcode 150 preparation

---
+++ neetcode/1.array/1.contains_duplicate.py
@@ -0,0 +1,9 @@
+def contains_duplicate(nums):
+    seen=set()
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+    return False
+
+print(contains_duplicate([1,5,2]))
